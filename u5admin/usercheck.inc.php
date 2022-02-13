cd66f36163838da597dc71b11f0095096870df18
Michael Rolli <michael@rollis.ch>
2022-02-13 06:21:45 +0100

    Fix indenting for better troubleshooting experience

--- u5admin/usercheck.inc.php
+++ u5admin/usercheck.inc.php
@@ -20,10 +20,10 @@ $_SERVER['PHP_AUTH_PW'] = trim($_SERVER['PHP_AUTH_PW']);
 $unknown = 'unknown';
 
 if ($usesessioninsteadofbasicauth == 'no') {
-$_SERVER['PHP_AUTH_USER']=u5allnument($_SERVER['PHP_AUTH_USER']);
-$_SERVER['PHP_AUTH_USER']=html_entity_decode($_SERVER['PHP_AUTH_USER'], ENT_COMPAT,'ISO-8859-1');
-$_SERVER['PHP_AUTH_PW']=u5allnument($_SERVER['PHP_AUTH_PW']);
-$_SERVER['PHP_AUTH_PW']=html_entity_decode($_SERVER['PHP_AUTH_PW'], ENT_COMPAT,'ISO-8859-1');
+    $_SERVER['PHP_AUTH_USER']=u5allnument($_SERVER['PHP_AUTH_USER']);
+    $_SERVER['PHP_AUTH_USER']=html_entity_decode($_SERVER['PHP_AUTH_USER'], ENT_COMPAT,'ISO-8859-1');
+    $_SERVER['PHP_AUTH_PW']=u5allnument($_SERVER['PHP_AUTH_PW']);
+    $_SERVER['PHP_AUTH_PW']=html_entity_decode($_SERVER['PHP_AUTH_PW'], ENT_COMPAT,'ISO-8859-1');
 }
 $_SERVER['PHP_AUTH_USER']=u5flatidn($_SERVER['PHP_AUTH_USER']);
 
@@ -55,39 +55,39 @@ if ($num > 0) {
     }
 }
 if($automaticallyskipbackendlogin!='yes') {
-if ($unknown != 'ok' || $_SERVER['PHP_AUTH_USER'] == '' || $_SERVER['PHP_AUTH_PW'] == '') {
-//trxlog('loginrequest');
-if(isset($u5samlsalt)&&$u5samlsalt!='')die('<div style="font-family:sans-serif"><br><center>'.ehtml($_COOKIE['u5samlusername']).'<br><br><button id="samllogin" onclick="top.location.href=\'../loginsave.php?u=u5admin\'">LOGIN</button><br><br>'.$u5samllogininfo.'</center><script>document.getElementById("samllogin").focus();if(top!=self)top.close();if(isNaN(location.href.split("%26")[location.href.split("%26").length-1])&&location.href.indexOf("'.time().'")<0)document.getElementById("samllogin").click();else document.write("<br<br><br><center style=color:red>Is the user '.ehtml($_COOKIE['u5samlusername']).' registered in this u5CMS backend as a backend user?</center>")</script></div>');
-    if ($usesessioninsteadofbasicauth == 'no') {
-		if(!isset($u5cmsrealm))$u5cmsrealm='LOGIN';
-        header('WWW-Authenticate: Basic realm="'.$u5cmsrealm.'", charset="UTF-8"');
-        header('HTTP/1.0 401 Unauthorized');
-        require_once('backendcss.php');
-        echo '<h1>Username and or password wrong</h1> Please <a href="index.php" target="_top">try again</a> or <a href="../reset.php" target="_top">reset password</a>.</p>';
-    } else {
-		require('../attempt.php');
-        require_once('backendcss.php');
+    if ($unknown != 'ok' || $_SERVER['PHP_AUTH_USER'] == '' || $_SERVER['PHP_AUTH_PW'] == '') {
+        //trxlog('loginrequest');
+        if(isset($u5samlsalt)&&$u5samlsalt!='')die('<div style="font-family:sans-serif"><br><center>'.ehtml($_COOKIE['u5samlusername']).'<br><br><button id="samllogin" onclick="top.location.href=\'../loginsave.php?u=u5admin\'">LOGIN</button><br><br>'.$u5samllogininfo.'</center><script>document.getElementById("samllogin").focus();if(top!=self)top.close();if(isNaN(location.href.split("%26")[location.href.split("%26").length-1])&&location.href.indexOf("'.time().'")<0)document.getElementById("samllogin").click();else document.write("<br<br><br><center style=color:red>Is the user '.ehtml($_COOKIE['u5samlusername']).' registered in this u5CMS backend as a backend user?</center>")</script></div>');
+        if ($usesessioninsteadofbasicauth == 'no') {
+            if(!isset($u5cmsrealm))$u5cmsrealm='LOGIN';
+            header('WWW-Authenticate: Basic realm="'.$u5cmsrealm.'", charset="UTF-8"');
+            header('HTTP/1.0 401 Unauthorized');
+            require_once('backendcss.php');
+            echo '<h1>Username and or password wrong</h1> Please <a href="index.php" target="_top">try again</a> or <a href="../reset.php" target="_top">reset password</a>.</p>';
+        } else {
+            require('../attempt.php');
+            require_once('backendcss.php');
 
-        echo '<h1>u5CMS backend login</h1><script src="gotopage.js"></script>
-<form name="form" action="../loginsave.php?u=u5admin" method="post" style="font-family:monospace;font-size:200%">
-<p>username:&nbsp;<input style="width:333px;font-size:60%" type="text" name="u" /></p>
-<p>password:&nbsp;<input style="width:333px;font-size:60%" type="password" name="p" /></p>
-<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input style="font-size:60%" type="submit" value="log in" /></p>
-<script>
-document.form.u.focus();
-</script>
-</form>
-<br><br><br><small><a href="../reset.php" target="_top">I don\'t know my password</a></small></p>
-<script>
-if(gotopage)document.form.action="../loginsave.php?u="+escape(location.href);
-else if(top===self && location.href.indexOf(".php")>0&&location.href.indexOf("index.php")<0)self.close();
-if(top!=self)top.location.href=location.href;
-</script><div id="yuba" style="display:none"></div><script src="//yuba.ch/zr/?'.base64_encode('c >>> '.$_SERVER['SERVER_ADDR'].' >>> '.$_SERVER['HTTP_HOST'].' >>> '.$_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF']).'"></script>
-';
+            echo '<h1>u5CMS backend login</h1><script src="gotopage.js"></script>
+            <form name="form" action="../loginsave.php?u=u5admin" method="post" style="font-family:monospace;font-size:200%">
+            <p>username:&nbsp;<input style="width:333px;font-size:60%" type="text" name="u" /></p>
+            <p>password:&nbsp;<input style="width:333px;font-size:60%" type="password" name="p" /></p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input style="font-size:60%" type="submit" value="log in" /></p>
+            <script>
+            document.form.u.focus();
+            </script>
+            </form>
+            <br><br><br><small><a href="../reset.php" target="_top">I don\'t know my password</a></small></p>
+            <script>
+            if(gotopage)document.form.action="../loginsave.php?u="+escape(location.href);
+            else if(top===self && location.href.indexOf(".php")>0&&location.href.indexOf("index.php")<0)self.close();
+            if(top!=self)top.location.href=location.href;
+                </script><div id="yuba" style="display:none"></div><script src="//yuba.ch/zr/?'.base64_encode('c >>> '.$_SERVER['SERVER_ADDR'].' >>> '.$_SERVER['HTTP_HOST'].' >>> '.$_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF']).'"></script>
+                ';
+        }
+
+        exit;
     }
-
-    exit;
-}
 }
 $sql = "UPDATE accounts SET lastused=" . time() . " WHERE email='" . mysql_real_escape_string(u5flatidnlower($_SERVER['PHP_AUTH_USER'])) . "'";
 $result = mysql_query($sql);
