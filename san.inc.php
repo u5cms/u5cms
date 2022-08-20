e858ac49449f0ac4e17678361a6b46c50b15c7af
Michael Rolli <michael@rollis.ch>
2022-08-20 07:46:12 +0200

    Sanitize get parameter q

--- san.inc.php
+++ san.inc.php
@@ -1,8 +1,10 @@
 <?php
+
 function u5stz($that) {
-return htmlspecialchars($that);
+    return htmlspecialchars($that);
 }
-$getq=$_GET['q'];
+
+$_GET['q'] = str_replace('\r','',str_replace('\n','',str_replace(';','',str_replace('<','',str_replace('>','',$getq)))));
 $_GET = array_map("u5stz", $_GET);
 $_COOKIE = array_map("u5stz", $_COOKIE);
 $_GET['q']=str_replace('<','',str_replace('>','',$getq));
