e258578814ecfdfca5c8a64efad297ce0b4d707b
Vũ Thanh Phan <vu.phan@adnovum.vn>
2023-05-19 17:15:02 +0700

    add language on publish page

--- chglang.inc.php
+++ chglang.inc.php
@@ -3,11 +3,15 @@
   global $lan1na;
   global $lan2na;
   global $lan3na;
+  global $lan4na;
+  global $lan5na;
   
   $u='?'.$_SERVER['QUERY_STRING'];
   $u=str_replace('l='.$lan1na,'',$u);
   $u=str_replace('l='.$lan2na,'',$u);
   $u=str_replace('l='.$lan3na,'',$u);
+  $u=str_replace('l='.$lan4na,'',$u);
+  $u=str_replace('l='.$lan5na,'',$u);
   $u=str_replace('l=','',$u);
   $u=($u.'&l='.$l);
   $u=str_replace('&&','&',$u);
