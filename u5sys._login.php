07bebd6f971a8101eca1dfe035da018ac15e1783
Michael Rolli <michael@rollis.ch>
2023-07-01 06:29:42 +0200

    Add new config option $u5samlinfrontend
    This enables turning of SAML login initaliztion in frontend requests if
    requested to do so AKA setting $u5samlinfrontend to "no".

--- u5sys._login.php
+++ u5sys._login.php
@@ -1,7 +1,9 @@
 <?php
 require_once('connect.inc.php');
 require_once('globalslogin.inc.php');
-if(isset($u5samlsalt)&&$u5samlsalt!='')die('<center>'.ehtml($_COOKIE['u5samlusername']).'<br><br><button id="samllogin" onclick="top.location.href=\'loginsave.php?u='.rawurlencode($_GET['u']).'\'">LOGIN</button><br><br>'.$u5samllogininfo.'</center><script>document.getElementById("samllogin").focus();if(isNaN(location.href.split("%26")[location.href.split("%26").length-1]))document.getElementById("samllogin").click();else document.write("<br<br><br><center style=color:red>Is the user '.ehtml($_COOKIE['u5samlusername']).' registered in this u5CMS backend as a backend user or inranet user or close user group member of the target page?</center>")</script>');
+if(isset($u5samlsalt)&&$u5samlsalt!='') {
+    if ($u5samlinfrontend != 'no') die('<center>'.ehtml($_COOKIE['u5samlusername']).'<br><br><button id="samllogin" onclick="top.location.href=\'loginsave.php?u='.rawurlencode($_GET['u']).'\'">LOGIN</button><br><br>'.$u5samllogininfo.'</center><script>document.getElementById("samllogin").focus();if(isNaN(location.href.split("%26")[location.href.split("%26").length-1]))document.getElementById("samllogin").click();else document.write("<br<br><br><center style=color:red>Is the user '.ehtml($_COOKIE['u5samlusername']).' registered in this u5CMS backend as a backend user or inranet user or close user group member of the target page?</center>")</script>');
+}
 ?>
 <h1 class="logintitle"><?php echo def($logintitle_d, $logintitle_e, $logintitle_f) ?></h1>
 <span class="loginintro"><?php echo def($loginintro_d, $loginintro_e, $loginintro_f) ?></span>
