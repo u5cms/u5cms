da9b5cd4c40a8e4378e9a37a6824c4f75224a999
Michael Rolli <michael@rollis.ch>
2023-10-27 16:22:06 +0200

    Set content encoding for message body (#90)
    * fix: add content encoding for message body
    
    * refactor: retab and strip whitespace

--- zendmail.php
+++ zendmail.php
@@ -5,7 +5,10 @@ require_once('u5admin/u5idn.inc.php');
 use Laminas\Mail\Message;
 
 $mail = new Message();
+// Set message encoding; this only affects headers!
 $mail->setEncoding('UTF-8');
+// Set the message content type for the body
+$mail->getHeaders()->addHeaderLine('Content-Type', 'text/plain; charset=UTF-8');
 $mail->addFrom(u5toidn($zendfrom), u5toutf8($zendname));
 $mail->addTo(u5toidn($zendto));
 $mail->setSubject(u5toutf8($zendsubject));
