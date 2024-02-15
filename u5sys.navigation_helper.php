f077fb654431bfb6b466bab8af827bf1a78a3a30
Michael Rolli <michael@rollis.ch>
2024-02-15 17:03:13 +0100

    fix: correctly render invisible unsorted list
    Now the unsorted list is invisible if its parent list item is not
    active.
    
    Fixes #98

--- u5sys.navigation_helper.php
+++ u5sys.navigation_helper.php
@@ -38,17 +38,23 @@ function navIsActiveLine($context) {
     return (strpos($context, $needle) === false) ? false : true;
 }
 
-function navGetVisibilityAttrib($context) {
+function navGetVisibilityAttrib($context, $activeUl = null) {
     if (empty($context)) {
         return '';
+    }
+    if (!is_null($activeUl) && !$activeUl) {
+        return ' style="display:none"';
     }
     return (navIsActiveLine($context)) ? '' : ' style="display:none"';
 }
 
 function navRenderLink($id, $title, $isActive = false) {
+    global $wasActive;
+
     // either id matches current page or isActive overriden from outside
     // the later is a specialty of layer 1 of the navigation
     $class = ($_GET['c'] == $id || $isActive) ? ' class="activeItem"' : '';
+    $wasActive = ($class == '') ? false : true;
     $link = '<a' . $class . ' href="index.php?c=' . $id . '&amp;l=' . $_GET['l'] . '">';
     $link.= render($title);
     $link.= '</a>';
