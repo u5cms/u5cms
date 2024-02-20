93562ffe0e772b5821b0c16b417498d8e2342e07
Stefan P. Minder <stemind@users.noreply.github.com>
2024-02-20 20:23:20 +0100

    For serial mail alert added in form
    For serial mail alert added in form if more than one e-mail address in from or to or cc or bcc.

---
+++ u5admin/mailingeditor.onchange.php
@@ -0,0 +1,1 @@
+onchange="if(this.value.indexOf(';')>-1 || this.value.indexOf(',')>-1){this.value='';alert('Only exactly one e-mail address is allowed in the field from which you come or - for the fields to, cc and bcc - an available variable in square brackets!')}"
