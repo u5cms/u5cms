fd71039326bfb0c8eb5825cc687886312bc61238
Stefan P. Minder <stemind@users.noreply.github.com>
2024-09-09 21:42:16 +0200

    Stop browser spinner early

--- u5admin/filter.inc.php
+++ u5admin/filter.inc.php
@@ -1,2 +1,5 @@
 <?php $h=sha1($username.$password.$_SERVER['PHP_AUTH_USER'].$_SERVER['PHP_AUTH_PW'].$sql_a)?>
-<iframe src="filter2.inc.php?sql=<?php echo rawurlencode($sql_a) ?>&h=<?php echo $h ?>" style="visibility:hidden" frameborder="0" width="0" height="0"></iframe>
+<iframe id="filter2inc" style="visibility:hidden" frameborder="0" width="0" height="0"></iframe>
+<script>
+setTimeout("document.getElementById('filter2inc').src='filter2.inc.php?sql=<?php echo rawurlencode($sql_a) ?>&h=<?php echo $h ?>'",3333);	
+</script>
