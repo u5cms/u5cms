efe397c3fb1732c029c52a48b93d24aa156ca290
Stefan P. Minder <stemind@users.noreply.github.com>
2025-01-30 20:12:45 +0100

    Better formatting index status message

--- indexupdate.inc.php
+++ indexupdate.inc.php
@@ -1,3 +1,3 @@
 <?php
-if(file_get_contents('fileversions/lastsave.txt')>file_get_contents('fileversions/lastindex.txt'))echo'<span lang="en" style="font-size:10px">Search index and sitemap are not yet fully updated, certain entries are as of '.date('Y-m-d H:i:s T',file_get_contents('fileversions/lastindex.txt')).'. The pending update will be carried out automatically later.</span>';
+if(file_get_contents('fileversions/lastsave.txt')>file_get_contents('fileversions/lastindex.txt'))echo'<div lang="en" style="font-size:10px;line-height:177%;margin-top:12px">Search index &amp; sitemap are not yet fully up to date. A few entries are still as of '.date('Y-m-d H:i T',file_get_contents('fileversions/lastindex.txt')).'. Auto-update will run soon.</div>';
 ?>
