5d68250c33d74420d3f4bf89f9a6f78c5b94e4d4
Stefan P. Minder <stemind@users.noreply.github.com>
2025-04-14 21:59:44 +0200

    Block manipulated meta data form URI

---
+++ u5admin/checktyp.inc.php
@@ -0,0 +1,3 @@
+<?php
+if($row_a['typ']!=$_GET['typ'])die('<center margin-top=77px><b style=color:red>The form is not displayed because the URL with which it was called up does not match the data record.</b></center>');
+?>
--- u5admin/metad.php
+++ u5admin/metad.php
@@ -36,7 +36,7 @@ $row_a = mysql_fetch_array($result_a);
   </p>
   <h2 style="margin-bottom:5px"><?php echo $lan1name ?><br />
   </h2>
-  <table id="lan1name" bgcolor="#FFFF99" width="100%">
+  <table id="lan1name" bgcolor="#FFFF99" width="100%"><?php require('checktyp.inc.php'); ?>
     <tr>
       <td>link&nbsp;text</td>
       <td width="99%"><input type="hidden" name="name" value="<?php echo $_GET['name']?>">
