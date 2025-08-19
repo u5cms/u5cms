51b54c94aed9e7c445ee38555b079dc1e4d32a3f
Stefan P. Minder <stemind@users.noreply.github.com>
2025-08-19 11:54:43 +0200

    Bugfix mass.php

--- u5admin/mass.php
+++ u5admin/mass.php
@@ -1,7 +1,6 @@
 <?php 
 require_once('connect.inc.php');
-$_POST['rowaid']=intval($_POST['rowaid']);
-$_POST['newstatus']=intval($_POST['newstatus']);
+require_once('../san.inc.php');
 trxlog('status '.$_POST['newstatus'].': '.str_replace('\'','',$_POST['rowaid']));
 $sql_a="UPDATE formdata SET lastmut='".time()."', status=".mysql_real_escape_string($_POST['newstatus'])." WHERE id IN (".($_POST['rowaid']).")";
 $result_a=mysql_query($sql_a);
