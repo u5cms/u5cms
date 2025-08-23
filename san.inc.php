51a374d51710aa0542999426497a8b3a0ba54b2f
Stefan P. Minder <stemind@users.noreply.github.com>
2025-08-23 23:01:35 +0200

    Stronger Input Validation
    Experimental, needs thorough testing

--- san.inc.php
+++ san.inc.php
@@ -7,3 +7,10 @@ $filterChars = array('\r', '\n', ';', '<', '>', '(', ')');
 $_GET['q'] = str_replace($filterChars, '', $_GET['q']);
 $_GET = array_map("u5stz", $_GET);
 $_COOKIE = array_map("u5stz", $_COOKIE);
+
+$keys = ['c','n','l','typ','name','id'];
+foreach ($keys as $k) {
+    $_GET[$k]  = isset($_GET[$k])  ? preg_replace('/[^a-z0-9!]/i','',$_GET[$k])  : '';
+    $_POST[$k] = isset($_POST[$k]) ? preg_replace('/[^a-z0-9!]/i','',$_POST[$k]) : '';
+}
+
