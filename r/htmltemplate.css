2dbc2ff0b18cf009823ef937c86c383d29c3fdc8
Stefan P. Minder <stemind@users.noreply.github.com>
2025-08-24 19:03:22 +0200

    Better Templates for Accessibility

--- r/htmltemplate.css
+++ r/htmltemplate.css
@@ -2,41 +2,58 @@
 {{{meta}}}
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
 <link href="favicon.ico" rel="shortcut icon" />
-<link rel="stylesheet" href="r/cssbase.css" type="text/css" />
-<link rel="stylesheet" href="js/jquery.fancybox.min.css" />
+<link rel="stylesheet" href="r/cssbase.css?1756071642" type="text/css" />
+<link rel="stylesheet" href="js/jquery.fancybox.min.css?1756071642" />
 <script src="js/jquery.js"></script>
 <script src="js/jquery.fancybox.min.js"></script>
 <script src="js/u5_scripts.js" type="text/javascript"></script>
-<script src="r/jsmobilespecific.css"></script>
+<script src="r/jsmobilespecific.css?1756071642"></script>
 </head>
-<body id="body"> 
-<section class="skipLinks">{{{skiplinks}}}</section>
-<main id="main">
-  <header id="header" style="background:white;background:url([_logo_]);background-size:cover">
-    <section id="metanavi"><div id="search"><a name="search"></a>{{{search}}}</div>
-      <section id="languages">{{{languages}}}</section>
-    </section>
-      <section id="slogan">{{{slogan}}}</section>
-  </header>
-  <!-- <section id="navigationtop">
-    <nav id="navTop"><a name="navigation"></a>{{{navigation}}}</nav> 
-  </section> REMOVING HTML COMMENT TAG HERE = SWITCH SPLIT NAVIGATION ON. FURTHER: RENAME id="navLeft" TO id="navLeftSubTop" HERE IN THIS WINDOW! -->
+<body id="body">
+  <nav class="skipLinks" aria-label="skip-links">
+    <a href="#content">Zum Inhalt springen</a>
+  </nav>
 
-<section id="outer">
-<section id="navigation"><nav id="navLeft" style="float:left"><a name="navigation"></a>{{{navigation}}}</nav></section>
-<article id="content"><a name="content"></a>{{{content}}}</article>
-<aside id="news">{{{right}}}</aside>
-<footer id="footer"><a name="contact"></a>{{{footer}}}</footer>
-</section>  
-</main>
-<script>
-function autoequalsone() {
-if(location.href.indexOf('auto=1')>0)location.reload();
-}
-setTimeout("autoequalsone()",2222);
-</script>
-<script>
-if(typeof u5mkmobile==="function" && !document.getElementById('nevermobile'))u5mkmobile();
-</script>
+  <div id="main">
+    <header id="header" style="background:white;background:url([_logo_]);background-size:cover">
+      <div id="metanavi">
+        <div id="search">{{{search}}}</div>
+        <div id="languages">{{{languages}}}</div>
+      </div>
+      <div id="slogan">{{{slogan}}}</div>
+    </header>
+
+    <!--
+    <div id="navigationtop">
+      <nav id="navTop">{{{navigation}}}</nav>
+    </div>
+    -->
+
+    <div id="outer">
+      <div id="navigation">
+        <nav id="navLeft" aria-label="Hauptnavigation">{{{navigation}}}</nav>
+      </div>
+
+      <main id="content" tabindex="-1">
+        {{{content}}}
+      </main>
+
+      <aside id="news">
+        {{{right}}}
+      </aside>
+
+      <footer id="footer">
+        {{{footer}}}
+      </footer>
+    </div>
+  </div>
+
+  <script>
+    function autoequalsone() {
+      if (location.href.indexOf('auto=1') > 0) location.reload();
+    }
+    setTimeout(autoequalsone, 2222);
+    if (typeof u5mkmobile === "function" && !document.getElementById('nevermobile')) u5mkmobile();
+  </script>
 </body>
 </html> 
