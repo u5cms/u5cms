7fb20f1b84f40f894f02af897966ba3c582d8302
Stefan P. Minder <stemind@users.noreply.github.com>
2025-08-28 00:09:17 +0200

    Refixes for Retesters

--- u5admin/mass.php
+++ u5admin/mass.php
@@ -1,6 +1,8 @@
 <?php 
 require_once('connect.inc.php');
+require('t2.php');
 $_POST['newstatus']=intval($_POST['newstatus']);
+$_POST['rowaid'] = trim(preg_replace('/[^0-9,]+/', '', $_POST['rowaid']), ',');
 trxlog('status '.$_POST['newstatus'].': '.str_replace('\'','',$_POST['rowaid']));
 $sql_a="UPDATE formdata SET lastmut='".time()."', status=".mysql_real_escape_string($_POST['newstatus'])." WHERE id IN (".($_POST['rowaid']).")";
 $result_a=mysql_query($sql_a);
