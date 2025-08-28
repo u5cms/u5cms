d50a6f16d7c2d840f56dc26d62996868411d9744
Stefan P. Minder <stemind@users.noreply.github.com>
2025-08-28 15:59:21 +0200

    No touch POST on MG edge cases

--- san.inc.php
+++ san.inc.php
@@ -8,9 +8,18 @@ $_GET['q'] = str_replace($filterChars, '', $_GET['q']);
 $_GET = array_map("u5stz", $_GET);
 $_COOKIE = array_map("u5stz", $_COOKIE);
 
+$skipPost = false;
+foreach (get_included_files() as $f) {
+    if (in_array(basename($f), ['formsave.php','delformsave.php','editformsave.php'])) {
+        $skipPost = true;
+        break;
+    }
+}
+
 $keys = ['c','n','l','typ','name','id'];
 foreach ($keys as $k) {
     $_GET[$k]  = isset($_GET[$k])  ? preg_replace('/[^a-z0-9!]/i','',$_GET[$k])  : '';
-    $_POST[$k] = isset($_POST[$k]) ? preg_replace('/[^a-z0-9!]/i','',$_POST[$k]) : '';
+    if (!$skipPost) {
+        $_POST[$k] = isset($_POST[$k]) ? preg_replace('/[^a-z0-9!]/i','',$_POST[$k]) : '';
+    }
 }
-
