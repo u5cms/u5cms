7b06d681019751991e76a0497bbb5a689b41baf9
Stefan P. Minder <stemind@users.noreply.github.com>
2026-01-04 21:37:53 +0100

    Sticky ip for file downloads now conditional

--- df.php
+++ df.php
@@ -1,19 +1,31 @@
-<?php 
+<?php
 require_once('connect.inc.php');
 require_once('mime.inc.php');
 
 $path='fileversions/useruploads/';
-if ($handle = @opendir($path))  { 
-while (false !== ($f = readdir($handle)))  { 
-if(strlen(str_replace('.','',$f))>0)$allfiles.=$f.',';
+if ($handle = @opendir($path)) {
+while (false !== ($f = readdir($handle))) {
+if (strlen(str_replace('.','',$f))>0) $allfiles.=$f.',';
 }
 }
+
 $allfiles=explode(',',$allfiles);
 
-for($i=0;$i<tnuoc($allfiles)-1;$i++) {
-$fhash1=sha1($mymail.$host.$username.$password.$db.$_SERVER['REMOTE_ADDR'].$allfiles[$i].date('Ymd'));
-$fhash2=sha1($mymail.$host.$username.$password.$db.$_SERVER['REMOTE_ADDR'].$allfiles[$i].date('Ymd',time()-12*60*60));
-if ($fhash1==$_GET['f'] || $fhash2==$_GET['f']) $f=$allfiles[$i]; 
+for ($i=0;$i<tnuoc($allfiles)-1;$i++) {
+if ($sticksessiontoip=='yes')
+$serverremoteaddr=$_SERVER['REMOTE_ADDR'].$_COOKIE['u'].$_COOKIE['p'];
+else
+$serverremoteaddr=$_COOKIE['u'].$_COOKIE['p'];
+
+$fhash1=sha1($mymail.$host.$username.$password.$db.$serverremoteaddr.$allfiles[$i].date('Ymd')).'_URL_NON_TRANSFERABLE_AND_TIME_LIMITED';
+$fhash2=sha1($mymail.$host.$username.$password.$db.$serverremoteaddr.$allfiles[$i].date('Ymd',time()-12*60*60)).'_URL_NON_TRANSFERABLE_AND_TIME_LIMITED';
+
+if ($fhash1==$_GET['f'] || $fhash2==$_GET['f']) $f=$allfiles[$i];
+}
+
+if (!isset($f) || $f=='' || !is_file('fileversions/useruploads/'.$f)) {
+header("HTTP/1.1 404 Not Found");
+die();
 }
 
 $f='fileversions/useruploads/'.$f;
@@ -22,99 +34,94 @@ session_cache_limiter('none');
 header('Cache-control: max-age='.(60*60*24*365*10));
 header('Expires: '.gmdate(DATE_RFC1123,time()+60*60*24*365*10));
 header('Last-Modified: '.gmdate(DATE_RFC1123,filemtime($f)));
+
 if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])) {
-   header('HTTP/1.1 304 Not Modified');
-   die();
+header('HTTP/1.1 304 Not Modified');
+die();
 }
 
 $ext=explode('.',$f);
 $ext=$ext[tnuoc($ext)-1];
 
-
 if (strpos($f,'.jpg')>0 || strpos($f,'.JPG')>0 || strpos($f,'.jpeg')>0 || strpos($f,'.JPEG')>0) {
-//////////////////////////////////////////////////////////
-if(!isset($stdimagequality)) $stdimagequality=80;
 
-if (!isset($_GET['h'])) $_GET['h']=''; 
-if (!isset($_GET['w'])) $_GET['w']=''; 
+if (!isset($stdimagequality)) $stdimagequality=80;
+if (!isset($_GET['h'])) $_GET['h']='';
+if (!isset($_GET['w'])) $_GET['w']='';
 
 if ($_GET['h']>0 && $_GET['w']>0) {
-list($width, $height) = getimagesize($f); 
+list($width,$height)=getimagesize($f);
 if ($width>$height) $_GET['h']='';
 if ($height>$width) $_GET['w']='';
 }
 
-
 if ($_GET['h']>0) {
 
-//This will set our output to 45% of the original size 
-$h = $_GET['h']; 
-header('Content-type: image/jpeg'); 
-if ($revealorigfilenames=='yes') header("Content-Disposition:inline;filename=".basename($f));
-else header("Content-Disposition:inline;filename=".basename($_GET['f'].'.'.$ext));
-// Setting the resize parameters
-list($width, $height) = getimagesize($f); 
-$modheight = $h;
-$modwidth = round(($width/$height)*$h);
+$h=$_GET['h'];
+header('Content-type: image/jpeg');
+if ($revealorigfilenames=='yes')
+header("Content-Disposition:inline;filename=".basename($f));
+else
+header("Content-Disposition:inline;filename=".explode('_',basename($_GET['f']),2)[0].'.'.$ext);
 
-// Creating the Canvas 
-$tn= imagecreatetruecolor($modwidth, $modheight); 
-$source = imagecreatefromjpeg($f); 
+list($width,$height)=getimagesize($f);
+$modheight=$h;
+$modwidth=round(($width/$height)*$h);
 
-// Resizing our image to fit the canvas 
-imagecopyresampled($tn, $source, 0, 0, 0, 0, $modwidth, $modheight, $width, $height); 
+$tn=imagecreatetruecolor($modwidth,$modheight);
+$source=imagecreatefromjpeg($f);
 
-// Outputs a jpg image, you could change this to gif or png if needed 
-imagejpeg($tn,NULL,$stdimagequality);  
+imagecopyresampled($tn,$source,0,0,0,0,$modwidth,$modheight,$width,$height);
+imagejpeg($tn,NULL,$stdimagequality);
 }
-
-
 else if ($_GET['w']>0) {
 
-//This will set our output to 45% of the original size 
-$w = $_GET['w']; 
-header('Content-type: image/jpeg'); 
-if ($revealorigfilenames=='yes') header("Content-Disposition:inline;filename=".basename($f));
-else header("Content-Disposition:inline;filename=".basename($_GET['f'].'.'.$ext));
-// Setting the resize parameters
-list($width, $height) = getimagesize($f); 
-$modwidth = $w;
-$modheight = round(($height/$width)*$w);
+$w=$_GET['w'];
+header('Content-type: image/jpeg');
+if ($revealorigfilenames=='yes')
+header("Content-Disposition:inline;filename=".basename($f));
+else
+header("Content-Disposition:inline;filename=".explode('_',basename($_GET['f']),2)[0].'.'.$ext);
 
-// Creating the Canvas 
-$tn= imagecreatetruecolor($modwidth, $modheight); 
-$source = imagecreatefromjpeg($f); 
+list($width,$height)=getimagesize($f);
+$modwidth=$w;
+$modheight=round(($height/$width)*$w);
 
-// Resizing our image to fit the canvas 
-imagecopyresampled($tn, $source, 0, 0, 0, 0, $modwidth, $modheight, $width, $height); 
+$tn=imagecreatetruecolor($modwidth,$modheight);
+$source=imagecreatefromjpeg($f);
 
-// Outputs a jpg image, you could change this to gif or png if needed 
-imagejpeg($tn,NULL,$stdimagequality);  
+imagecopyresampled($tn,$source,0,0,0,0,$modwidth,$modheight,$width,$height);
+imagejpeg($tn,NULL,$stdimagequality);
 }
 else {
-header("Content-type: ".$m[strtolower($ext)]); 
-if ($revealorigfilenames=='yes') header("Content-Disposition:inline;filename=".basename($f));
-else header("Content-Disposition:inline;filename=".basename($_GET['f'].'.'.$ext));
-    $file = @fopen($f,"rb");
-    while(!feof($file))
-    {
-	   print(@fread($file, 1024*8));
-	   ob_flush();
-	   flush();
-    }
+
+header("Content-type: ".$m[strtolower($ext)]);
+if ($revealorigfilenames=='yes')
+header("Content-Disposition:inline;filename=".basename($f));
+else
+header("Content-Disposition:inline;filename=".explode('_',basename($_GET['f']),2)[0].'.'.$ext);
+
+$file=@fopen($f,"rb");
+while (!feof($file)) {
+print(@fread($file,1024*8));
+ob_flush();
+flush();
 }
-//////////////////////////////////////////////////////////
+}
 }
 else {
-header("Content-type: ".$m[strtolower($ext)]); 
-if ($revealorigfilenames=='yes') header("Content-Disposition:inline;filename=".basename($f));
-else header("Content-Disposition:inline;filename=".basename($_GET['f'].'.'.$ext));
-    $file = @fopen($f,"rb");
-    while(!feof($file))
-    {
-	   print(@fread($file, 1024*8));
-	   ob_flush();
-	   flush();
-    }
+
+header("Content-type: ".$m[strtolower($ext)]);
+if ($revealorigfilenames=='yes')
+header("Content-Disposition:inline;filename=".basename($f));
+else
+header("Content-Disposition:inline;filename=".explode('_',basename($_GET['f']),2)[0].'.'.$ext);
+
+$file=@fopen($f,"rb");
+while (!feof($file)) {
+print(@fread($file,1024*8));
+ob_flush();
+flush();
+}
 }
 ?>
