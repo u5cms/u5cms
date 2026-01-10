0833d7730193f1b6585c60c5e10bb99060b23c51
Stefan P. Minder <stemind@users.noreply.github.com>
2026-01-10 14:04:56 +0100

    Add files via upload

--- u5sys.search.php
+++ u5sys.search.php
@@ -7,7 +7,7 @@ if (isset($_GET['c']) && (substr($_GET['c'],0,1)=='!' || $_GET['c']=='_searchsi'
   <input type="hidden" name="l" value="<?php echo $_GET['l']?>" />
   <input type="hidden" name="c" value="_search" />
 
-  <input id="search_Input" type="text" name="q" value="" aria-label="<?php echo def($recherche_1,$recherche_2,$recherche_3,$recherche_4,$recherche_5) ?>" />
+  <input <?php include('search.onchange.inc.php') ?> id="search_Input" type="text" name="q" value="" aria-label="<?php echo def($recherche_1,$recherche_2,$recherche_3,$recherche_4,$recherche_5) ?>" />
   <input onclick="document.getElementById('search_Input').value=document.getElementById('search_Input').value.replace(/Site \+ Intranet/g,'')" id="search_Submit" aria-label="<?php echo def($recherche_1,$recherche_2,$recherche_3,$recherche_4,$recherche_5) ?>" type="submit" class="btnSubmit" value="" />
 </form>
 <script type="text/javascript">
